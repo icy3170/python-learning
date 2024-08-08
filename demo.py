199a5701eb7240d91dd35002c6de30c1e431edc8
ryanchen <ryanchen1008@yeah.net>
2024-08-08 13:53:36 +0800

    test1

---
+++ demo.py
@@ -0,0 +1,12 @@
+import random
+
+a = random.randint(1,999)
+b = int(input('input a number from 1 to 999:'))
+while True:
+    if a>b:
+        b = (int(input('less than the number,keep on input!:')))
+    elif a<b:
+        b = (int(input('more than the number,keep on input!:')))
+    else:
+        print('you r right!,the number is:',a)
+        break
